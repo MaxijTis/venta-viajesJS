cd544dfe88f6a2105f19fd6cf5d69b37f76f4ac8
Maximiliano Tis <maxi_tis@hotmail.com>
2024-04-07 22:00:24 -0300

    Obteniendo elementos del DOM e importando el script de ciudades

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+import{ barcelona, roma, paris, londres } from './ciudades'
+ 
+//Obtener los elementos del DOM
+let enlaces = document.querySelectorAll('a');
+let tituloElemento = document.getElementById('titulo');
+let subtituloElemento = document.getElementById('subtitulo');
+let parrafoElemento = document.getElementById('parrafo');
+
+
+
 
